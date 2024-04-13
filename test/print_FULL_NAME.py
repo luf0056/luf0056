9ad8b3278a829ffce55e4734fd1e3d338868c745
luf0056 <lucasime31@gmail.com>
2024-04-13 11:03:27 +0800

    test commit

---
+++ test/print_FULL_NAME.py
@@ -0,0 +1,7 @@
+"""This module defines a person's name and prints their full name in uppercase."""
+import sklearn
+
+FIRST_NAME = "Luca"
+LAST_NAME = "Simeone"
+full_name = f"{FIRST_NAME} {LAST_NAME}"
+print(full_name.upper())
