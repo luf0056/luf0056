8742c7988458319eb9d2b62eded629d5e4d51d27
luf0056 <lucasime31@gmail.com>
2024-04-13 11:35:20 +0800

    Removed test files

--- test/print_FULL_NAME.py
+++
@@ -1,8 +0,0 @@
-"""This module defines a person's name and prints their full name in uppercase."""
-import sklearn
-
-FIRST_NAME = "Luca"
-LAST_NAME = "Simeone"
-DOB = "31/07.86"
-full_name = f"{FIRST_NAME} {LAST_NAME} {DOB}"
-print(full_name.upper())
